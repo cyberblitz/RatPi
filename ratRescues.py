503ce20a94ff76e0f0de9773a24f07f4391d029a
cyberblitz <42084666+cyberblitz@users.noreply.github.com>
2019-07-04 07:21:54 +1000

    Updated API extract and LCD code

--- ratRescues.py
+++ ratRescues.py
@@ -1,26 +1,43 @@
 import requests
+import datetime
 from time import sleep
 import time
-import datetime
-
-#import I2C_LCD_Driver
-#mylcd = I2C_LCD_Driver.lcd()
-
-# Fuel Rat api Rescue URL
-rescue_url = "https://api.fuelrats.com/rescues"
-headers = {"Authorization": "Bearer Key"}
+from threading import Thread
+import I2C_LCD_Driver
+mylcd = I2C_LCD_Driver.lcd()
 
 
-def get_data(url):
-    try:
-        data = requests.get(url, headers=headers).json()
-        data = data['data']
-        return [dta['attributes'] for dta in data if dta['attributes']['status'] == 'open']
-    except requests.exceptions.RequestException:
-        sleep(5)
-        data = requests.get(url, headers=headers).json()
-        data = data['data']
-        return [dta['attributes'] for dta in data if dta['attributes']['status'] == 'open']
+json_dataG = []
+mylcd.lcd_clear()
+
+def get_data():
+
+    global json_dataG
+    while True:
+        try:
+            fin = []
+            rescue_url = "https://api.fuelrats.com/rescues?[status]=open"  # ?$not[status]=closed"
+            headers = {"Authorization": "Bearer Key"}
+            data = requests.get(rescue_url, headers=headers).json()
+            data = data['data']
+            json_data = [dta['attributes'] for dta in data]
+            if json_data:
+                for each in json_data:
+                    fin_val = ["C: %s" % each['client'],
+                               "P: %s" % each['platform'] + " CR: %s" % each['codeRed'],
+                               "S: %s" % each['system'],
+                               "A: %s" % dte_convert(each['createdAt'])]
+                    fin.append(fin_val)
+            else:
+                fin = None
+            if fin != json_dataG:
+                json_dataG = fin
+            sleep(5)
+
+        except requests.exceptions.Timeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            continue
 
 
 # function to convert string datetime value when rescue commenced (usually entered by Mecha)
@@ -35,40 +52,70 @@ def dte_convert(dte):
     return ret
 
 
-while True:
-    json_data = get_data(rescue_url)
-
-    if json_data:
-        for each in json_data:
-
-            client = each['client']
-            platform = each['platform']
-            code_red = each['codeRed']
-            system = each['system']
-            active = dte_convert(each['createdAt'])
-
-            print("C: %s" % client)
-            print("P: %s" % platform + " CR: %s" % code_red)
-            print("S: %s" % system)
-            print("A: %s" % str(active))
-
-#            mylcd.lcd_display_string("C: %s" %client ,1)
-#            mylcd.lcd_display_string("P: %s" %platform + "CR: %s" %str(cde_red), 2)
-#            mylcd.lcd_display_string("S: %s" %system, 1)
-#            mylcd.lcd_display_string("A: %s" %str(res_datetime), 2)
-
-    else:
-            print("Date: %s" % time.strftime("%d/%m/%Y"))
-            print("Time: %s" % time.strftime("%H:%M"))
-            print('No Active Rescues')
-
-    sleep(5)
-
-#         #   mylcd.lcd_display_string("Date: %s" %time.strftime("%d/%m/%Y"), 1)
-#         #   mylcd.lcd_display_string("Time: %s" %time.strftime("%H:%M:%S"), 2)
-#           # mylcd.lcd_display_string('No Active Rescues', 3)
 
 
+def clock():
+        fin_val = ("Date: %s" % time.strftime("%d/%m/%Y"),
+                   "Time: %s" % time.strftime("%H:%M:%S"),
+                   'No Active Rescues',
+                   '        <^__)~')
+
+        return fin_val
+
+def lcd_scroll(ln3, num_cols):
+    sleep(0.5)
+    mylcd.lcd_display_string(ln3[:num_cols],3)
+    for i in range(len(ln3) - num_cols + 1):
+        text_to_print = "S: " + ln3[i:i+num_cols]
+        mylcd.lcd_display_string(text_to_print,3)
+        #time.sleep(0.2)
+
+def lcd_data(in_data):
+    num_cols = 17
+    
+    ln1, ln2, ln3, ln4 = in_data
+
+       
+    mylcd.lcd_display_string(ln1, 1)
+    mylcd.lcd_display_string(ln2, 2)
+    mylcd.lcd_display_string(ln3[:20], 3)
+    mylcd.lcd_display_string(ln4, 4)
+    
+    if(len(ln3) > num_cols):
+        ln3.replace("S: ", "")
+        lcd_scroll(ln3, num_cols)
+
+    #print(ln1)
+    #print(ln2)
+    #print(ln3)
+    #print(ln4)
 
 
+def main():
+    i = 0
+    varHold = ()
+    while True:
+        try:
+            global json_dataG
+            t_clock = clock()
+            if json_dataG is not None:
+                i = 1
+                for data in json_dataG:
+                    lcd_data(data)
+                    sleep(3)
+                    data3 = data[1:3]
+                    if data3 != varHold:
+                        mylcd.lcd_clear()
+                    varHold = data3
+            else:
+                if i > 0:
+                    mylcd.lcd_clear()
+                    i = 0
+                lcd_data(t_clock)
+        except ValueError:
+            continue
 
+t1 = Thread(target=get_data)
+t2 = Thread(target=main)
+t1.start()
+t2.start()
